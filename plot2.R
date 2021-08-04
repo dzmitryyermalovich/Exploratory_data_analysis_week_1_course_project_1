eebf228b502f0d1d0f7b2e92810661371caaa624
dzmitryyermalovich <79224710+dzmitryyermalovich@users.noreply.github.com>
2021-08-04 15:05:19 +0300

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,16 @@
+df<-read.table("C:\\Education\\R_programming\\exdata_data_household_power_consumption\\household_power_consumption.txt",header=TRUE,sep=";")
+df1<-subset(df,Date==("1/2/2007"))
+df2<-subset(df,Date==("2/2/2007"))           
+df3<-rbind(df1,df2)
+
+df3$Global_active_power<-apply(df3[3],2,as.numeric)
+df3$Sub_metering_1=as.numeric(df3$Sub_metering_1)
+df3$Sub_metering_2=as.numeric(df3$Sub_metering_2)
+df3$Sub_metering_3=as.numeric(df3$Sub_metering_3)
+df3$Voltage=as.numeric(df3$Voltage)
+df3$Global_reactive_power=as.numeric(df3$Global_reactive_power)
+DateTime<-strptime(paste(df3$Date,df3$Time,sep=" "),format = "%d/%m/%Y %H:%M:%S")
+
+png("plot2.png")
+with(df3,plot(DateTime,Global_active_power,type='l',xlab = "",ylab = "Global Active Power (kilowatts)"))
+dev.off()
